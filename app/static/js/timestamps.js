1789d726a21419ab83c490d9ea62520fe2aaca70
cider_sweet <victor.zakharenko@yahoo.com>
2020-10-14 17:24:49 +0300

    launch take fields input

--- app/static/js/timestamps.js
+++ app/static/js/timestamps.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var id = "input" + length;
         var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + id + "\"/>");
         var label = $("<label for=\"" + id + "\">" + 'timestamp'     + "</label>");
-        var input = $("<input type=\"text\" id=\"" + id + "\" name=\"" + id + "\" />");
+        var input = $("<input type=\"text\" class=\"input\" id=\"" + id + "\" name=\"" + id + "\" />");
         var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
         removeButton.click(function() {
             $(this).parent().remove();
@@ -16,6 +16,13 @@ $(document).ready(function() {
         $("#yourform").append(fieldWrapper);
     });
     $("#launch").click(function() {
-        console.log('hey dof    ')
+        var fieldSet = $("#yourform .input")
+        timestamps = []
+        fieldSet.each(function()  {
+            timestamps.push($(  this ).val());
+        })
+        console.log(timestamps);
     });
+
+    $("#add").trigger('click');
 });
